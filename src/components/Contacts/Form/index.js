8ff538eb46205b5baeb5040026af907852d77018
Zülal Cengiz <zc.swe@gmail.com>
2024-07-01 12:41:40 +0300

    form changes

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Form.css';
 function Form() {
+    const [form, setForm] = useState({ fullname: "", phone_number: "" });
+
+    const onChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
     return (
         <div class="container">
             <div>
-                <input name="fullname" placeholder="Full Name" />
+                <input name="fullname" placeholder="Full Name" onChange={onChangeInput}/>
             </div>
 
             <div>
-                <input name="phone_number" placeholder="Phone Number" />
+                <input name="phone_number" placeholder="Phone Number" onChange={onChangeInput} />
             </div>
 
             <div><button>Add</button></div>
